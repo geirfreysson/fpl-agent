f4725f0d8d62e6a39427cd364524d17eb3623947
Geir Freysson <geir@datasmoothie.io>
2025-08-10 09:01:32 +0100

    make initial suggestions resopnsive

--- frontend/components/assistant-ui/thread.tsx
+++ frontend/components/assistant-ui/thread.tsx
@@ -170,7 +170,7 @@ const ThreadWelcomeWithComposer: FC = () => {
 
 const ThreadWelcomeSuggestions: FC = () => {
   return (
-    <div className="mt-3 flex w-full items-stretch justify-center gap-4">
+    <div className="mt-1 grid w-full grid-cols-2 gap-2 md:mt-3 md:flex md:items-stretch md:justify-center md:gap-4">
       <ThreadPrimitive.Suggestion
         className="hover:bg-muted/80 flex max-w-sm grow basis-0 flex-col items-center justify-center rounded-lg border p-3 transition-colors ease-in"
         prompt="Who should I captain this week?"
